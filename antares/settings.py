fae4e0cef5c62d2ac697d525d2bcb8e22fbab941
Ashutosh S Gautam <ashutoshxo@users.noreply.github.com>
2026-08-24 11:37:10 +0530

    Configure S3 media storage for Django

--- antares/settings.py
+++ antares/settings.py
@@ -46,8 +46,20 @@ INSTALLED_APPS = [
     'ganymede',
     'paypal.standard.ipn',
     'rest_framework',
+    'storages',
 
 ]
+
+# AWS S3 Settings
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-north-1')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# Media Storage Configuration
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
