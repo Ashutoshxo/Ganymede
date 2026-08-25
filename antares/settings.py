ac9882e08c2b869bb98221c24919cdba2e669312
Ashutosh S Gautam <ashutoshxo@users.noreply.github.com>
2026-08-25 11:20:58 +0530

    Add env_file support to frontend service

--- antares/settings.py
+++ antares/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']  # Update this with your allowed hosts in production
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
